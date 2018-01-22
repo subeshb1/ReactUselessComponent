22044521e8917be115f715bffff27576c9f7a957
subeshb1 <subeshb1@gmail.com>
2018-01-22 20:32:45 +0545

    File setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    //entry Point
+    entry: path.resolve(__dirname) + '/src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist') + '/app',
+        filename: 'compact.js',
+        publicPath: '/app/'
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname,'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react','es2016']
+                }
+            },
+            {
+                rest: /\.css/,
+                include: path.resolve(__dirname,'src'),
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+
+}
