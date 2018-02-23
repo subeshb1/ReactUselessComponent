4e2b1d071d067896abbb182485a0fc9849bec55a
subeshb1 <subeshb1@gmail.com>
2018-02-23 09:13:09 +0545

    Made immutable

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -45,19 +45,71 @@ let tuples = {
   }
 };
 class Drawer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedItem: undefined
-        }
-        console.log(this.getContentData(tuples));
-    }
-    
-    setSelected(item) {
-        this.setState({selectedItem:item});
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: [...this.getContentData(tuples)],
 
+      // selectedItem: undefined,
+      // dragItem: undefined,
+      selectedItem: {
+        item: undefined,
+        index:undefined,
+        isDragged:false
 
+      }
+    };
+    console.log(this.getContentData(tuples));
+  }
+
+  setSelected(item) {
+    this.setState({ selectedItem: item });
+  }
+
+  update({selectedItem,dragItem}) { 
+    if(selectedItem !== null && dragItem !== null) {
+      this.setState({ selectedItem,dragItem });
+    } else if(selectedItem !== null ) {
+      this.setState({ selectedItem });
+    } else if(dragItem !== null ) {
+      this.setState({ dragItem });
+    } else
+      throw new TypeError ("None of the params match");
+  }
+
+  update1(selectedItem) {
+      if(selectedItem) {
+        let mappedContent = this.state.content.map (obj => Object.assign({},obj));
+
+        if(selectedItem.item && selectedItem.index !== undefined) {
+
+          if(this.state.selectedItem.item) {
+            mappedContent[this.state.selectedItem.index].isSelected = false;
+          }
+          mappedContent[selectedItem.index] = selectedItem.item;
+          
+        } else if(selectedItem.index !== undefined){
+          mappedContent[selectedItem.index].isSelected = false;
+          mappedContent[selectedItem.index]['sdsd'] = false;
+          selectedItem.index = undefined;
+          selectedItem.isDragged = false;
+          
+        } else 
+          throw new TypeError("Invalid selected Item");
+
+          
+        this.setState({content:mappedContent,selectedItem});
+      } else {
+        throw new TypeError("Excepts a defined param");
+      }
+  }
+
+  componentWillUpdate(np,ns) {
+    console.log(this.state.content,ns.content);
+  }
+  componentDidUpdate() {
+    console.log(this.state);
+  }
   render() {
     return (
       <div style={{ background: "#e7e7e7" }} className="container-fluid">
@@ -69,61 +121,67 @@ class Drawer extends Component {
           {/* Tools Menu */}
           <ToolBar />
           {/* Drawer */}
-          <Draw tuples={tuples} setSelected={this.setSelected.bind(this)} />
+          <Draw
+            content={this.state.content}
+            selectedItem={this.state.selectedItem}
+            setSelected={this.setSelected.bind(this)}
+            update={this.update.bind(this)}
+            update1={this.update1.bind(this)}
+          />
           {/*Settings*/}
-          <SettingsBar selectedItem={this.state.selectedItem}  setSelected={this.setSelected.bind(this)}/>
+          {/* <SettingsBar
+            selectedItem={this.state.selectedItem}
+            setSelected={this.setSelected.bind(this)}
+          /> */}
         </div>
       </div>
     );
   }
 
-
-
-
-
   getContentData(val) {
     let tuples = val;
     let y = 300;
     let x = -100;
-    
+
     let stateContent = [];
-    tuples.state.forEach( state=> {
-        stateContent.push({
-            type: "STATE",
-            name: state,
-            isDragged:false,
-            isSelected:false,
-            x:x+=200,
-            y:y,
-            isStart:tuples.initial == state,
-            isFinal: ! (tuples.final.findIndex( val => val === state) == -1)
-        });
+    tuples.state.forEach(state => {
+      stateContent.push({
+        type: "STATE",
+        name: state,
+        isSelected: false,
+        x: (x += 200),
+        y: y,
+        isStart: tuples.initial == state,
+        isFinal: !(tuples.final.findIndex(val => val === state) == -1)
+      });
     });
 
     let linkContent = [];
-    for(let state in tuples.transition) {
-        for(let input in tuples.transition[state]) {
-            let start = stateContent.find(s => s.name == state);
-            let end = stateContent.find(s => s.name == tuples.transition[state][input][0]);
-            let link = linkContent.find(link => link.start == start && link.end == end);
-            if(!link) {
-                linkContent.push ({
-                    type: "ARC",
-                    start,
-                    end,
-                    input : [input],
-                    isDragged:false,
-                    isSelected:false,
-                });
-             } else {
-                 link.input.push(input)
-             }
+    for (let state in tuples.transition) {
+      for (let input in tuples.transition[state]) {
+        let start = stateContent.find(s => s.name == state);
+        let end = stateContent.find(
+          s => s.name == tuples.transition[state][input][0]
+        );
+        let link = linkContent.find(
+          link => link.start == start && link.end == end
+        );
+        if (!link) {
+          linkContent.push({
+            type: "ARC",
+            start,
+            end,
+            input: [input],
+            isSelected: false
+          });
+        } else {
+          link.input.push(input);
         }
+      }
     }
 
-    return [...linkContent,...stateContent];
-
-}
+    return [...linkContent, ...stateContent];
+  }
 }
 
 Drawer.propTypes = {};
--- src/utils/svg/draw.js
+++ src/utils/svg/draw.js
@@ -9,14 +9,9 @@ class Draw extends Component {
         
         
         this.state = {
-            content: [
-                ...this.getContentData()
-            ],
-            
-            selectedItem:undefined,
-            dragItem:undefined,
-            scale:1,
-            touchAction:'auto'
+
+            scale:1,//k
+            touchAction:'auto'//k
         }
        
         
@@ -31,8 +26,7 @@ class Draw extends Component {
     //to move a Child (mouseMove)
     moveChild(e) {
         // e.preventDefault();
-        if(this.state.dragItem) {
-            this.props.setSelected(this.state.dragItem);
+        if(this.props.state.dragItem) {
             let rect = e.currentTarget.getBoundingClientRect();
             let clientX = e.clientX || e.touches[0].clientX;
             let clientY = e.clientY || e.touches[0].clientY;
@@ -40,9 +34,9 @@ class Draw extends Component {
             let offsetY = clientY - rect.top;
             // console.log(e);
            
-            this.state.dragItem.x = offsetX/this.state.scale;
-            this.state.dragItem.y = offsetY/this.state.scale;
-            
+            this.props.state.dragItem.x = offsetX/this.state.scale;
+            this.props.state.dragItem.y = offsetY/this.state.scale;
+            this.props.setSelected(this.props.state.dragItem);
             this.setState({dragItem:this.state.dragItem});
         }
         
@@ -53,9 +47,9 @@ class Draw extends Component {
 
     //Remove dragItem when mouse Released (mouseUp)
     removeDrag(e) {
-        if(this.state.dragItem) {
-            this.state.dragItem.isDragged=false
-            this.setState({dragItem: undefined,touchAction:'none'});
+        if(this.props.state.dragItem) {
+            this.props.state.dragItem.isDragged=false
+            this.props.update( {dragItem:this.props.state.dragItem.isDragged,selectedItem:null});
             
         }
         
@@ -63,24 +57,35 @@ class Draw extends Component {
 
     //set a item to be draged
     setDragItem(index) {
-        let dragItem = this.state.content[index];
-        if(this.state.selectedItem)
-            this.state.selectedItem.isSelected = false;
-        dragItem.isDragged = true;
-        dragItem.isSelected = true;
-        this.props.setSelected(dragItem);
-        this.setState({dragItem,selectedItem:dragItem,touchAction:'none'});
+        // console.log("Enter");
+        // let dragItem = this.props.state.content[index];
+        // if(this.props.state.selectedItem)
+        //     this.props.state.selectedItem.isSelected = false;
+        // dragItem.isDragged = true;
+        // dragItem.isSelected = true;
+        // this.props.update( {dragItem,selectedItem:dragItem});
+        // this.setState({touchAction:'none'});
         
+        let item = Object.assign({},this.props.content[index]);
+        let selectedItem = Object.assign({},this.props.selectedItem);
+       
+        selectedItem.index = index;
+        selectedItem.item = item;
+        item.isSelected = true;
+        selectedItem.isDragged = true;
+        this.props.update1( selectedItem);
+        this.setState({touchAction:'none'});
         
-    }    
+    }
+        
 
    //Remove Selection (onClick)
    removeSelection(e) {
        
-       if(this.state.selectedItem) {
-            this.state.selectedItem.isSelected = false;
+       if(this.props.state.selectedItem) {
+            this.props.state.selectedItem.isSelected = false;
             console.log("Removed",this.state);
-            this.setState({selectedItem:undefined,touchAction:'auto'});
+            this.setState({touchAction:'auto'});
             this.props.setSelected(undefined);
        }
    }
@@ -91,7 +96,6 @@ class Draw extends Component {
     }
 
     render() {
-
         let content = this.getContent();       
         return (
                 
@@ -111,11 +115,12 @@ class Draw extends Component {
                         className="position-absolute"
                         width="300%" height="300%"
                         style={{background: "white",touchAction:this.state.touchAction,top:0,left:0}} 
-                        onMouseMove={this.moveChild} 
-                        onMouseUp={this.removeDrag}
-                        onTouchEnd={this.removeDrag}
-                        onMouseDown= {this.removeSelection}
-                        onTouchMove={this.moveChild}>
+                        // onMouseMove={this.moveChild} 
+                        // onMouseUp={this.removeDrag}
+                        // onTouchEnd={this.removeDrag}
+                        // onMouseDown= {this.removeSelection}
+                        // onTouchMove={this.moveChild}
+                        >
                     
                     <g transform={`scale(${this.state.scale})`}>
                         {content}
@@ -138,13 +143,12 @@ class Draw extends Component {
 
 
     getContent() {
-        return this.state.content.map((val,index) => {
+        return this.props.content.map((val,index) => {
             if(val.type == 'STATE') {
                 return (
                 <State key={index} 
                     cx={val.x} cy={val.y} 
                     index={index} text={val.name} 
-                    isDragged={val.isDragged}
                     isSelected={val.isSelected}
                     onMouseDown={this.setDragItem}
                     isStart={val.isStart}
@@ -170,49 +174,7 @@ class Draw extends Component {
 
     }
 
-    getContentData() {
-        let tuples = this.props.tuples;
-        let y = 300;
-        let x = -100;
-        
-        let stateContent = [];
-        tuples.state.forEach( state=> {
-            stateContent.push({
-                type: "STATE",
-                name: state,
-                isDragged:false,
-                isSelected:false,
-                x:x+=200,
-                y:y,
-                isStart:tuples.initial == state,
-                isFinal: ! (tuples.final.findIndex( val => val === state) == -1)
-            });
-        });
-
-        let linkContent = [];
-        for(let state in tuples.transition) {
-            for(let input in tuples.transition[state]) {
-                let start = stateContent.find(s => s.name == state);
-                let end = stateContent.find(s => s.name == tuples.transition[state][input][0]);
-                let link = linkContent.find(link => link.start == start && link.end == end);
-                if(!link) {
-                    linkContent.push ({
-                        type: "ARC",
-                        start,
-                        end,
-                        input : [input],
-                        isDragged:false,
-                        isSelected:false,
-                    });
-                 } else {
-                     link.input.push(input)
-                 }
-            }
-        }
-
-        return [...linkContent,...stateContent];
-
-    }
+    
 
 }
 
